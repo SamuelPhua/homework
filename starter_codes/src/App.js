c17c76d8fcbf186efed4bc1903989ec461c8ec00
samuelphua <samuel.phua@hotmail.com>
2023-01-10 00:23:29 +0800

    homework

---
+++ starter_codes/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import weatherData from "../Components/weatherData";
+import weatherIcons from "../Components/weatherIcons";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>My WeatherIcons App</h1>
+    </div>
+  );
+}
