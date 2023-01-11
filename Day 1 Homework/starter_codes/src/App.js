9d3b7e9c4d8168d861da585ba30491150adb7e35
samuelphua <samuel.phua@hotmail.com>
2023-01-11 13:00:17 +0800

    Day 3 Homework

--- Day 1 Homework/starter_codes/src/App.js
+++ Day 1 Homework/starter_codes/src/App.js
@@ -6,31 +6,18 @@ import WeatherIcon from "./components/WeatherIcon";
 import WeatherData from "./components/WeatherData";
 
 export default function App() {
-  const [name, setName] = useState("Eugene");
-  const [show, setShow] = useState(true);
-
   return (
     <div className="App">
       <h1>{name} WeatherIcons App</h1>
       <section>
-        {show ? (
-          weatherData.map((value, index) => (
-            <WeatherIcons key={index}>
-              <WeatherIcon thePropName={value.image} />
-              <WeatherData conditions={value.conditions} time={value.time} />
-            </WeatherIcons>
-          ))
-        ) : (
-          <p>no icon</p>
-        )}
-        {/* {weatherData.map((value, index) => (
+        {weatherData.map((value, index) => (
           <WeatherIcons
             img={value.img}
             conditions={value.conditions}
             time={value.time}
             key={index}
           />
-        ))} */}
+        ))}
       </section>
       <button onClick={() => setShow((prevState) => !prevState)}>
         Change Show
---
+++ Samuel's Day 3 Homework/starter_codes/src/Components/ProductName.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+
+// const ProductName = (props) => {
+//   const [name, setName] = useState("");
+
+//   const inputName = (name) => {
+//     setName(name);
+//   };
+
+//   return (
+//     <div>
+//       {props.products.map((value, index) => {
+//         <div
+//           className="AllTheThings"
+//           type="text"
+//           name={inputName}
+//           key={index}
+//           value={name}
+//         />;
+//       })}
+//     </div>
+//   );
+// };
+
+// export default ProductName;
